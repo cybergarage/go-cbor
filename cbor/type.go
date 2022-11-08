ee3258d7549afd5586f3b0295701a75ff0e4b2cb
Satoshi Konno <skonno@cybergarage.org>
2022-11-08 21:42:29 +0900

    Add type.go

---
+++ cbor/type.go
@@ -0,0 +1,28 @@
+// Copyright (C) 2022 The go-cbor Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cbor
+
+type MajorType byte
+
+const (
+	Uint   MajorType = 0x00
+	Negint MajorType = 0x20
+	Bytes  MajorType = 0x40
+	Text   MajorType = 0x60
+	Array  MajorType = 0x80
+	Map    MajorType = 0xA0
+	Tag    MajorType = 0xC0
+	Float  MajorType = 0xE0
+)
