4572a882dbafef53d3110f32c71e6c5bdc378aa9
Satoshi Konno <skonno@cybergarage.org>
2022-11-10 13:06:59 +0900

    Add (read/append)Int16Bytes()

--- cbor/func.go
+++ cbor/func.go
@@ -18,6 +18,10 @@ import (
 	"fmt"
 )
 
+////////////////////////////////////////////////////////////
+// int8
+////////////////////////////////////////////////////////////
+
 func readInt8Bytes(src []byte) (int8, []byte, error) {
 	srcLen := len(src)
 	if srcLen < 1 {
@@ -25,6 +29,16 @@ func readInt8Bytes(src []byte) (int8, []byte, error) {
 	}
 	return int8(src[0]), src[1:], nil
 }
+
+func appendInt8Bytes(buf []byte, val int8) []byte {
+	return append(buf,
+		byte(val),
+	)
+}
+
+////////////////////////////////////////////////////////////
+// uint8
+////////////////////////////////////////////////////////////
 
 func readUint8Bytes(src []byte) (uint8, []byte, error) {
 	srcLen := len(src)
@@ -34,40 +48,41 @@ func readUint8Bytes(src []byte) (uint8, []byte, error) {
 	return src[0], src[1:], nil
 }
 
+func appendUint8Bytes(buf []byte, val uint8) []byte {
+	return append(buf,
+		val,
+	)
+}
+
+////////////////////////////////////////////////////////////
+// int16
+////////////////////////////////////////////////////////////
+
+func readInt16Bytes(src []byte) (int16, []byte, error) {
+	srcLen := len(src)
+	if srcLen < 2 {
+		return 0, nil, fmt.Errorf(errorInvalidIntegerBytes, src)
+	}
+	return (int16(src[0])<<8 | int16(src[1])), src[2:], nil
+}
+
+func appendInt16Bytes(buf []byte, val int16) []byte {
+	return append(buf,
+		byte(val>>8),
+		byte(val),
+	)
+}
+
+////////////////////////////////////////////////////////////
+// uint16
+////////////////////////////////////////////////////////////
+
 func readUint16Bytes(src []byte) (uint16, []byte, error) {
 	srcLen := len(src)
 	if srcLen < 2 {
 		return 0, nil, fmt.Errorf(errorInvalidIntegerBytes, src)
 	}
 	return (uint16(src[0])<<8 | uint16(src[1])), src[2:], nil
-}
-
-func readUint32Bytes(src []byte) (uint32, []byte, error) {
-	srcLen := len(src)
-	if srcLen < 4 {
-		return 0, nil, fmt.Errorf(errorInvalidIntegerBytes, src)
-	}
-	return (uint32(src[0])<<24 | uint32(src[1])<<16 | uint32(src[2])<<8 | uint32(src[3])), src[4:], nil
-}
-
-func readUint64Bytes(src []byte) (uint64, []byte, error) {
-	srcLen := len(src)
-	if srcLen < 8 {
-		return 0, nil, fmt.Errorf(errorInvalidIntegerBytes, src)
-	}
-	return (uint64(src[0])<<56 | uint64(src[1])<<48 | uint64(src[2])<<40 | uint64(src[3])<<32 | uint64(src[4])<<24 | uint64(src[5])<<16 | uint64(src[6])<<8 | uint64(src[7])), src[8:], nil
-}
-
-func appendInt8Bytes(buf []byte, val int8) []byte {
-	return append(buf,
-		byte(val),
-	)
-}
-
-func appendUint8Bytes(buf []byte, val uint8) []byte {
-	return append(buf,
-		val,
-	)
 }
 
 func appendUint16Bytes(buf []byte, val uint16) []byte {
@@ -77,6 +92,18 @@ func appendUint16Bytes(buf []byte, val uint16) []byte {
 	)
 }
 
+////////////////////////////////////////////////////////////
+// uint32
+////////////////////////////////////////////////////////////
+
+func readUint32Bytes(src []byte) (uint32, []byte, error) {
+	srcLen := len(src)
+	if srcLen < 4 {
+		return 0, nil, fmt.Errorf(errorInvalidIntegerBytes, src)
+	}
+	return (uint32(src[0])<<24 | uint32(src[1])<<16 | uint32(src[2])<<8 | uint32(src[3])), src[4:], nil
+}
+
 func appendUint32Bytes(buf []byte, val uint32) []byte {
 	return append(buf,
 		byte(val>>24),
@@ -84,6 +111,18 @@ func appendUint32Bytes(buf []byte, val uint32) []byte {
 		byte(val>>8),
 		byte(val),
 	)
+}
+
+////////////////////////////////////////////////////////////
+// uint64
+////////////////////////////////////////////////////////////
+
+func readUint64Bytes(src []byte) (uint64, []byte, error) {
+	srcLen := len(src)
+	if srcLen < 8 {
+		return 0, nil, fmt.Errorf(errorInvalidIntegerBytes, src)
+	}
+	return (uint64(src[0])<<56 | uint64(src[1])<<48 | uint64(src[2])<<40 | uint64(src[3])<<32 | uint64(src[4])<<24 | uint64(src[5])<<16 | uint64(src[6])<<8 | uint64(src[7])), src[8:], nil
 }
 
 func appendUint64Bytes(buf []byte, val uint64) []byte {
