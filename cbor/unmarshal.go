10a8f41ecef8328e89c9670bbaf061f23bac7dc6
Satoshi Konno <skonno@cybergarage.org>
2022-11-13 22:02:41 +0900

    Update Unmarshal()

---
+++ cbor/unmarshal.go
@@ -0,0 +1,25 @@
+// Copyright (C) 2022 The go-cbor Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cbor
+
+import (
+	"bytes"
+)
+
+// Unmarshal parses the specified CBOR-encoded bytes and returns the data representation of Go.
+func Unmarshal(cborBytes []byte, v any) (any, error) {
+	decoder := NewDecoder(bytes.NewReader(cborBytes))
+	return decoder.Decode()
+}
