148c96d018109a0cfed96d65800c46b607771246
Satoshi Konno <skonno@cybergarage.org>
2023-05-22 01:21:00 +0900

    Update Decoder::unmarshalArrayTo()

--- cbor/config.go
+++ cbor/config.go
@@ -16,15 +16,13 @@ package cbor
 
 // Config represents a configuration for CBOR encoder and decoder.
 type Config struct {
-	MapSortEnabled       bool
-	TypeRetentionEnabled bool
+	MapSortEnabled bool
 }
 
 // NewConfig returns a new config instance.
 func NewConfig() *Config {
 	return &Config{
-		MapSortEnabled:       false,
-		TypeRetentionEnabled: true,
+		MapSortEnabled: false,
 	}
 }
 
@@ -36,14 +34,4 @@ func (config *Config) SetMapSortEnabled(flag bool) {
 // IsMapSortEnabled returns true whether the map keys are sorted.
 func (config *Config) IsMapSortEnabled() bool {
 	return config.MapSortEnabled
-}
-
-// SetTypeRetentionEnabled sets a flag to retain the type information.
-func (config *Config) SetTypeRetentionEnabled(flag bool) {
-	config.TypeRetentionEnabled = flag
-}
-
-// IsTypeRetentionEnabled returns true whether the type information is retained.
-func (config *Config) IsTypeRetentionEnabled() bool {
-	return config.TypeRetentionEnabled
 }
